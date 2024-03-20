d6be8cf77cf1fba30aeb5c24ca924c06f7f912b9
Nuradil Zhasulan <nurikzhasulan96@gmail.com>
2024-03-20 20:48:37 +0500

    отправка на почту

--- app.js
+++ app.js
@@ -5,17 +5,10 @@ const valueRoutes = require("./routes/values");
 const priceRoutes = require("./routes/prices");
 const countExitRoutes = require("./routes/countExits");
 const extraServicesRoutes = require("./routes/extraService");
+const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(express.json());
-
-// const corsOptions = {
-//   origin: "http://10.224.0.16:3001", // Разрешить только запросы с этого источника
-
-//   optionsSuccessStatus: 200, // некоторые браузеры устарели
-// };
-
-// app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   const allowedOrigins = ['http://localhost:3001', 'https://mighty-caverns-86737-6af8502f2aff.herokuapp.com'];
@@ -28,6 +21,62 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
+});
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.mail.ru',
+  port: 465, // Обычно для SMTPS (SMTP over SSL) используется порт 465
+  secure: true, // true для 465 порта, false для других портов
+  auth: {
+    user: 'nurik_22_11_96@mail.ru',
+    pass: 'kzHuYEbLr6Ndj44a0Ykm',
+  },
+});
+
+// Endpoint для отправки электронных писем
+app.post('/send-email', (req, res) => {
+  const { email, phone, totalPrice, boilerType,
+    boilerPower1,
+    boilerPower2,
+    waterSource,
+    waterVolume,
+    selectedCount,
+    sendCheckedState,
+    sendExtraServices,
+    contour,
+    power,
+    gasConsumption } = req.body;
+
+  const mailOptions = {
+    from: 'eplotorg63@mail.ru',
+    to: 'eplotorg63@mail.ru',
+    subject: 'Новый заказ',
+    text: `Поступил новый заказ. 
+    Email клиента: ${email}, 
+    телефон: ${phone}, 
+    итоговая сумма: ${totalPrice}, 
+    комбинацию котлов: ${boilerType},
+    мощность котлов 1: ${boilerPower1},
+    мощность котлов 2: ${boilerPower2},
+    горячая вода: ${waterSource},
+    бойлер: ${waterVolume},
+    кол. потребителей тепла: ${selectedCount},
+    выбранные потребителей тепла: ${JSON.stringify(sendCheckedState)},
+    доп. заказы: ${JSON.stringify(sendExtraServices)},
+    характеристики: "Котел": ${contour},
+    характеристики: "Мощность котельной кВт": ${power},
+    характеристики: "Расход газа природного м3/ч": ${gasConsumption}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Ошибка при отправке письма');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Письмо успешно отправлено');
+    }
+  });
 });
 
 
