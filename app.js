8ce2edbd31e5b8641b006fb18a109d46329e5b92
JassulanNuradil <37346327+NuradilZhassulan@users.noreply.github.com>
2024-03-25 16:58:05 +0500

    Update app.js
    port поменял

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ app.use("/api/extraservices", extraServicesRoutes);
 
 app.use("/api/systemDescription", systemDescriptionRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
