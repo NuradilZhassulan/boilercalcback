63da90160fc06309b4f138d11035b05b853ce26c
JassulanNuradil <37346327+NuradilZhassulan@users.noreply.github.com>
2024-03-25 17:32:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ app.use("/api/extraservices", extraServicesRoutes);
 
 app.use("/api/systemDescription", systemDescriptionRoutes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
